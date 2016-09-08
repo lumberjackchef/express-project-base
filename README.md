314ca91b1d78ef9c1c72830a0b72078c6ec522f8
Ryan Pearson <rayn@ryanpearson.net>
2016-09-07 17:56:53 -0700

    Project init

---
+++ README.md
@@ -0,0 +1,30 @@
+# Pivotal Tracker - Multi-Project Account Data Graphing
+
+---
+
+## Installation:
+- clone this repo
+- `$ npm install`
+
+#### Production Environment:
+- `$ NODE_ENV=production nodemon`
+
+#### Development Environment: 
+- `$ nodemon`
+
+---
+
+## Features:
+- ES2015 on the backend
+- Pug Templating
+- Nodemon for server handling
+- Refactors the Express project base for code efficiency
+
+---
+
+## Notes:
+- Add ES2015 for backend
+- Get all projects in [time frame]
+- Get total bugs from those projects
+- Get total bug rejections from those projects
+- Graph it all somehow
---
+++ app.js
@@ -0,0 +1,65 @@
+"use strict";
+
+let express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+
+    routes = require('./routes/index'),
+    users = require('./routes/users'),
+
+    app = express();
+
+app
+
+// view engine setup
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'pug')
+
+// uncomment after placing your favicon in /public
+//.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+  .use(logger('dev'))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(cookieParser())
+  .use(express.static(path.join(__dirname, 'public')))
+
+  .use('/', routes)
+  .use('/users', users)
+
+// catch 404 and forward to error handler
+  .use((req, res, next) => {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  })
+;
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
